6aabf66caa2ced137c7d2ce35e68508dea3e5994
Peter Marklund <peter@marklunds.com>
2017-12-22 20:04:39 +0100

    Bumped to version 0.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'type_spec',
     packages = ['type_spec'],
-    version = '0.1',
+    version = '0.2',
     description = 'Pre/post condition type checks for python functions',
     author = 'Peter Marklund',
     author_email = 'peter@marklunds.com',
     url = 'https://github.com/peter/type_spec',
-    download_url = 'https://github.com/peter/type_spec/archive/0.1.tar.gz',
+    download_url = 'https://github.com/peter/type_spec/archive/0.2.tar.gz',
     keywords = ['types', 'typing', 'testing', 'contract'],
     classifiers = [],
 )
